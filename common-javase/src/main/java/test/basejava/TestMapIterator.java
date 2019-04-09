5603e40d13e7f45f275d75af54dcac9e179f5c92
17637900215 <wxx521215>
2019-04-10 00:01:39 +0800

    统一环境

--- common-javase/src/main/java/test/basejava/TestMapIterator.java
+++ common-javase/src/main/java/test/basejava/TestMapIterator.java
@@ -2,6 +2,8 @@ package test.basejava;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -84,5 +86,14 @@ public class TestMapIterator {
         System.out.println("第四种遍历方式耗时：-》"+(end4-begin4));
     }
 
+
+    @Test
+    public  void test(){
+        Date date = new Date();
+        System.out.println(date);
+        SimpleDateFormat  simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        String format = simpleDateFormat.format(date);
+        System.out.println(format);
+    }
 
 }
